774d380241a02ab8475022cc3df75d83a2ce42a1
Mike McGuire <MoonshotMike@gmail.com>
2018-03-25 16:35:19 -0600

    add line on tester branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ class App extends Component {
         </p>
         <p>
           Give me that high-paying developer job right now...
-          Do these changes commit?
+        </p>
+        <p>
+          I'm adding this line to the tester branch only.
         </p>
       </div>
     );
